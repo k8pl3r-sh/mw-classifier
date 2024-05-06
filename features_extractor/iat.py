64f3ea803341378e6805607a9c9706b6434c3531
k8pl3r-sh <k8pl3r@gmail.com>
2024-05-06 07:57:23 +0200

    Add IAT extractor

---
+++ features_extractor/iat.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import pefile
+import argparse
+
+DEFAULT_FILE = "snake_deluxe.exe"
+
+def extract_iat(filename):
+    pe = pefile.PE(filename)
+    pe.parse_data_directories()
+    iat = []
+    extracted = {}
+    for entry in pe.DIRECTORY_ENTRY_IMPORT:
+        for imp in entry.imports:
+            iat.append(imp.name.decode("utf-8"))
+        extracted[entry.dll.decode("utf-8")] = iat
+    return extracted
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Extract Import Address Table (IAT) from a PE file.')
+    parser.add_argument('filename', nargs='?', default=DEFAULT_FILE, help='Path to the PE file (default: %(default)s)')
+
+    args = parser.parse_args()
+
+    if args.filename == DEFAULT_FILE:
+        print("Using default file: snake_deluxe.exe")
+        print("Please specify with the -f flag to extract the IAT from a different binary.")
+
+    iat = extract_iat(args.filename)
+    print(iat)
