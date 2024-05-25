f184e806ea6d8ade6354fc9d881e999d8e0a274c
k8pl3r-sh <k8pl3r@gmail.com>
2024-05-25 18:16:59 +0200

    Refactoring

--- Visualize-trends/code/interactive_versions_of_code/listing_9_15.py
+++
@@ -1,9 +0,0 @@
-import pandas
-import seaborn
-from matplotlib import pyplot
-
-malware = pandas.read_csv("malware_data.csv")
-axis = seaborn.violinplot(x=malware['type'], y=malware['positives'])
-axis.set(xlabel="Malware type", ylabel="Number of vendor detections",
-         title="Number of detections by malware type")
-pyplot.show()
---
+++ logger.py
@@ -0,0 +1,21 @@
+import logging
+
+class LoggingFormatter(logging.Formatter):
+    # Colors
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    gray = "\x1b[38m"
+    # Styles
+    reset = "\x1b[0m"
+    bold = "\x1b[1m"
+
+    COLORS = {
+        logging.DEBUG: gray + bold,
+        logging.INFO: blue + bold,
+        logging.WARNING: yellow + bold,
+        logging.ERROR: red,
+        logging.CRITICAL: red + bold,
+    }
