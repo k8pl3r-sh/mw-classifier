e603796a73236460dd0482ab8b1870fd578c09bb
k8pl3r-sh <k8pl3r@gmail.com>
2024-08-30 12:54:24 +0200

    Add if name == main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from malware_similarity_neo4j.similarity_engine import SimilarityEngine
 from utils.tools import load_yml
 
 
-config = load_yml("config.yml")
-
-sim = SimilarityEngine(config)
-sim.run(target_directory="data/", save=True)
+if __name__ == "__main__":
+    config = load_yml("config.yml")
+    sim = SimilarityEngine(config)
+    sim.run(target_directory="data/", save=True)
