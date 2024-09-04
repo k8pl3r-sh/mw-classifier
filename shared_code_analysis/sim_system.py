9706960974084a05d4d435a3a4cc870d798a4df7
k8pl3r-sh <k8pl3r@gmail.com>
2024-09-04 16:40:37 +0200

    Remove comments

--- shared_code_analysis/sim_system.py
+++ shared_code_analysis/sim_system.py
@@ -7,33 +7,6 @@ import shelve
 import sys
 from numpy import array
 
-"""
-Copyright (c) 2015, Joshua Saxe
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source train_model must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name 'Joshua Saxe' nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL JOSHUA SAXE BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
 NUM_MINHASHES = 256
 NUM_SKETCHES = 8
 
@@ -56,6 +29,7 @@ def get_database():
     """
     dbpath = os.path.join(os.path.dirname(__file__), 'samples.db')
     return shelve.open(dbpath, protocol=2, writeback=True)
+
 
 def getstrings(fullpath):
     """
