33e1e41ad19de1e279978aebc9610b60e6468cf4
k8pl3r-sh <k8pl3r@gmail.com>
2024-09-13 10:16:13 +0200

    Remove black text color for better lisibility

--- utils/logger.py
+++ utils/logger.py
@@ -25,7 +25,7 @@ class LoggingFormatter(logging.Formatter):
     def format(self, record: object) -> str:
         log_color = self.COLORS.get(record.levelno, self.reset)
         format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
-        format = format.replace("(black)", self.black + self.bold)
+        format = format.replace("(black)", self.bold) # removed 'self.black +' because can't be seen in terminal
         format = format.replace("(reset)", self.reset)
         format = format.replace("(levelcolor)", log_color)
         format = format.replace("(green)", self.green + self.bold)
