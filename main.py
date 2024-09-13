f1609febd615a2ae1ed522c8c65c131590e5d138
k8pl3r-sh <k8pl3r@gmail.com>
2024-09-13 11:28:03 +0200

    Add elapsed time count

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 
 from malware_similarity_neo4j.similarity_engine import SimilarityEngine
 from utils.tools import load_yml
+from time import time
 
 
 if __name__ == "__main__":
+    start_time = time()
     config = load_yml("config.yml")
     sim = SimilarityEngine(config)
     sim.run(target_directory="SAMPLES/APT1_MALWARE_FAMILIES/", save=False)
+    end_time = time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
