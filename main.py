7b8834ec31fe10f692fbc835a5210eab2b9ac22e
k8pl3r-sh <k8pl3r@gmail.com>
2024-09-15 17:46:27 +0200

    .

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     end_time = time()
     elapsed_time = end_time - start_time
     print(f"Elapsed time: {elapsed_time:.2f} seconds")
-    # sim.similarity_matrix_heatmap('similarity_matrix_test.png')
+    sim.similarity_matrix_heatmap('similarity_matrix_minhash.png')
 
 if __name__ == "__main__":
     # TODO : add argparse to do --debug and --optimize
