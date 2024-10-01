d80432fd247d5bb75d906c3d1af895dc2e5c0c63
k8pl3r-sh <k8pl3r@gmail.com>
2024-10-01 15:26:27 +0200

    .

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class Main:
         end_time = time()
         elapsed_time = end_time - start_time
         self.log.info(f"Elapsed time: {elapsed_time:.2f} seconds")
-        sim.similarity_matrix_heatmap('test_debug.png')
+        #sim.similarity_matrix_heatmap('test_debug.png')
 
 if __name__ == "__main__":
     # TODO : add argparse to do --debug and --optimize
