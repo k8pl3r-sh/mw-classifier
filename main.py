5c140783382eb8cc5f75a0b025e11ceada0e3ca1
k8pl3r-sh <k8pl3r@gmail.com>
2024-10-02 19:42:10 +0200

    linting

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from time import time
 from memory_profiler import profile
 from utils.logger import Log
 
+
 class Main:
     def __init__(self):
         self.config = load_yml("config.yml")
         self.log = Log("Main", self.config)
 
         pass
+
     @profile
     def main(self):
         start_time = time()
-
 
         sim = SimilarityEngine(self.config)
         sim.run(target_directory="SAMPLES/APT1_MALWARE_FAMILIES/", save=False, sampling=True)
@@ -24,7 +25,8 @@ class Main:
         end_time = time()
         elapsed_time = end_time - start_time
         self.log.info(f"Elapsed time: {elapsed_time:.2f} seconds")
-        #sim.similarity_matrix_heatmap('test_debug.png')
+        # sim.similarity_matrix_heatmap('test_debug.png')
+
 
 if __name__ == "__main__":
     # TODO : add argparse to do --debug and --optimize
