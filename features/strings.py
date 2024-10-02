07cd4aebf671260d798d2180551b2839f1e6a323
k8pl3r-sh <k8pl3r@gmail.com>
2024-10-02 16:05:44 +0200

    Manage Exceptions

--- features/strings.py
+++ features/strings.py
@@ -2,8 +2,6 @@
 import os
 from utils.logger import Log
 
-DEFAULT_FILE = "snake_deluxe.exe"
-
 
 class Strings:
     def __init__(self, config: dict):
@@ -12,7 +10,7 @@ class Strings:
     def __repr__(self):
         return "Strings"
 
-    def extract(self, filename: str) -> dict:
+    def extract(self, filename: str) -> dict | None:
         """
         Extract strings from the binary indicated by the 'filename' parameter, and then return the set of unique strings in
         the binary.
@@ -24,7 +22,11 @@ class Strings:
         -------
 
         """
-        strings = os.popen("strings '{0}'".format(filename)).read()
-        strings = set(strings.split("\n"))
-        self.log.debug(f"Extracted {len(strings)} strings from {filename}")
-        return {'strings': strings}
+        try:
+            raw_strings = os.popen("strings '{0}'".format(filename)).read()
+            strings = set(raw_strings.split("\n"))
+            self.log.debug(f"Extracted {len(strings)} strings from {filename}")
+            return {'strings': strings}
+        except Exception as e:
+            self.log.error(f"Error extracting strings from {filename} : {e}")
+            return None
